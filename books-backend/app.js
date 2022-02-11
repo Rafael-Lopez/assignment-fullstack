1a63d3cd2dd6d81555e67eb871a662d31a9ebfb6
Rafael Lopez <lopez.rafael08@gmail.com>
2022-02-10 21:44:01 -0500

    Enable CORS in backend project

--- books-backend/app.js
+++ books-backend/app.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
