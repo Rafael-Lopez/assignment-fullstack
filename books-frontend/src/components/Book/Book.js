d1249f03a853de711d77f0de26db8956e640a9fc
Rafael Lopez <lopez.rafael08@gmail.com>
2022-02-11 00:26:01 -0500

    Add Delete button to Book component

--- books-frontend/src/components/Book/Book.js
+++ books-frontend/src/components/Book/Book.js
@@ -1,7 +1,23 @@
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Book = ( {book} ) => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleDelete= (id) => {
+        axios.delete( process.env.REACT_APP_API_URL + "/books/" + id)
+            .then( response => {
+                setShow(false);
+            });
+    };
+
     return (
         <Card className='m-2'>
             <Card.Body>
@@ -9,8 +25,27 @@ const Book = ( {book} ) => {
                 <Card.Text>Author: {book.author}</Card.Text>
                 <Card.Text>Year: {book.publishingYear}</Card.Text>
                 <Card.Text>ISBN: {book.isbnNumber}</Card.Text>
-                <Button variant="primary">View</Button>
+                <Container>
+                <Row className="justify-content-md-center">
+                    <Col><Button variant="danger" className='w-100' onClick={handleShow}>Delete</Button></Col>
+                </Row></Container>
             </Card.Body>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Are you sure you want to delete this book?</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="danger" onClick={() => handleDelete(book.id)}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </Card>
     );
 }
